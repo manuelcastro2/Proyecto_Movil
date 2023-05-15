5555976b99316581ff60d4f03c53c615622a3d8a
manuelcastro2 <malavermanuelricardo@gmail.com>
2023-05-14 22:08:46 -0500

    calendario ya casi sale

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Calendar } from 'react-native-calendars';
+import moment from 'moment';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class CalendarExample extends React.Component {
+    state = {
+        markedDates: {},
+        isStartDatePicked: false,
+        isEndDatePicked: false,
+        startDate: ''
+    }
+
+    onDayPress = (day) => {
+        console.log(day);
+        if (this.state.isStartDatePicked == false) {
+            let markedDates = {}
+            markedDates[day.dateString] = { color: '#00B0BF', textColor: '#FFFFFF' };
+            this.setState({
+                markedDates: markedDates,
+                startDate: day.dateString,
+            });
+        } else {
+            let markedDates = this.state.markedDates
+            let startDate = moment(this.state.startDate);
+            let endDate = moment(day.dateString);
+            let range = endDate.diff(startDate, 'days')
+            if (range > 0) {
+                let tempDate = startDate.add(1, 'day');
+                tempDate = moment(tempDate).format('YYYY-MM-DD')
+                markedDates[tempDate] = { color: '#00B0BF', textColor: '#FFFFFF' };
+                this.setState({
+                    markedDates: markedDates,
+                    startDate: ''
+                });
+            } else {
+                alert('Select an upcomming date!');
+            }
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Calendar
+                    monthFormat={"MMMM yyyy"}
+                    markedDates={this.state.markedDates}
+                    markingType="period"
+                    hideExtraDays={true}
+                    hideDayNames={true}
+                    onDayPress={this.onDayPress}
+                />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: 'rgba(52, 52, 52, 0.8)',
+        padding: 20,
+        justifyContent: 'center'
+    }
 });
---
+++ Components/Calender-push/calenderPull.js
@@ -0,0 +1,5 @@
+//firebase
+//https://www.npmjs.com/package/react-native-calendars
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Calendar,CalendarList,Agenda } from 'react-native-calendars';
