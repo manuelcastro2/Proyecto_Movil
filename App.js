4d5ab23f4310b6338d136a90123857144971e758
manuelcastro2 <malavermanuelricardo@gmail.com>
2023-05-16 16:30:42 -0500

    calendario mas simplificado

--- App.js
+++ App.js
@@ -7,47 +7,25 @@ export default class CalendarExample extends React.Component {
     state = {
         markedDates: {},
         isStartDatePicked: false,
-        isEndDatePicked: false,
-        startDate: ''
     }
 
     onDayPress = (day) => {
-        console.log(day);
+        console.log(day.dateString)
         if (this.state.isStartDatePicked == false) {
             let markedDates = {}
             markedDates[day.dateString] = { color: '#00B0BF', textColor: '#FFFFFF' };
             this.setState({
                 markedDates: markedDates,
-                startDate: day.dateString,
             });
-        } else {
-            let markedDates = this.state.markedDates
-            let startDate = moment(this.state.startDate);
-            let endDate = moment(day.dateString);
-            let range = endDate.diff(startDate, 'days')
-            if (range > 0) {
-                let tempDate = startDate.add(1, 'day');
-                tempDate = moment(tempDate).format('YYYY-MM-DD')
-                markedDates[tempDate] = { color: '#00B0BF', textColor: '#FFFFFF' };
-                this.setState({
-                    markedDates: markedDates,
-                    startDate: ''
-                });
-            } else {
-                alert('Select an upcomming date!');
-            }
-        }
+        } 
     }
 
     render() {
         return (
             <View style={styles.container}>
                 <Calendar
-                    monthFormat={"MMMM yyyy"}
                     markedDates={this.state.markedDates}
                     markingType="period"
-                    hideExtraDays={true}
-                    hideDayNames={true}
                     onDayPress={this.onDayPress}
                 />
             </View>
