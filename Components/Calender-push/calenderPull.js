936ebbbb02f71d0bbb1df77efb9e234a38eced4f
manuelcastro2 <malavermanuelricardo@gmail.com>
2023-05-16 22:46:38 -0500

    agregando otro calendario

--- Components/Calender-push/calenderPull.js
+++ Components/Calender-push/calenderPull.js
@@ -1,5 +1,44 @@
 //firebase
-//https://www.npmjs.com/package/react-native-calendars
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Calendar,CalendarList,Agenda } from 'react-native-calendars';
+import * as React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Calendar } from 'react-native-calendars';
+import moment from 'moment';
+
+export default class CalendarExample extends React.Component {
+    state = {
+        markedDates: {},
+        isStartDatePicked: false,
+    }
+
+    onDayPress = (day) => {
+        console.log(day.dateString)
+        if (this.state.isStartDatePicked == false) {
+            let markedDates = {}
+            markedDates[day.dateString] = { color: '#00B0BF', textColor: '#FFFFFF' };
+            this.setState({
+                markedDates: markedDates,
+            });
+        } 
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Calendar
+                    markedDates={this.state.markedDates}
+                    markingType="period"
+                    onDayPress={this.onDayPress}
+                />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'rgba(52, 52, 52, 0.8)',
+        padding: 20,
+        justifyContent: 'center'
+    }
+});
