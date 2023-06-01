80017f112f53763761d4b51d698a976422d8c346
DESKTOP-BNNIPES\USUARIO <sergioherreravel@gmail.com>
2023-06-01 16:23:23 -0500

    Arreglando base de datos

--- Components/baseDeDatos.js
+++ Components/baseDeDatos.js
@@ -8,137 +8,141 @@ import firebase from './../firebase-config';
 
 const llamadoData = ({ navigation }) => {
 
-    const sendDataToFirebase = async (userId, phone, name, age) => {
-        try {
-            const firestore = getFirestore();
-            await setDoc(doc(firestore, 'test', userId), {
-                phone: phone,
-                name: name,
-                age: age,
-            });
-            console.log('Datos guardados en Firebase.');
-            // Aquí puedes añadir cualquier otra acción que desees realizar después de guardar los datos.
-        } catch (error) {
-            console.log('Error al guardar los datos: ', error);
-        }
-        clearInputs();
-    };
+  const sendDataToFirebase = async (correo, date, meta, estado) => {
+    try {
+
+      const idDocumento = correo + "_" + date + "_" + meta;
+      const firestore = getFirestore();
+      await setDoc(doc(firestore, 'test', idDocumento), {
+        correo: correo,
+        date: date,
+        meta: meta,
+        estado: estado
+      });
+      console.log('Datos guardados en Firebase exitosamente.');
+      console.log(idDocumento);
+      // Aquí puedes añadir cualquier otra acción que desees realizar después de guardar los datos.
+    } catch (error) {
+      console.log('Error al guardar los datos: ', error);
+    }
+    clearInputs();
+  };
 
 
-    const [userId, setUserId] = useState('');
-    const [name, setName] = useState('');
-    const [age, setAge] = useState('');
-    const [phone, setPhone] = useState('');
+  const [correo, setCorreo] = useState('');
+  const [date, setDate] = useState('');
+  const [meta, setMeta] = useState('');
+  const [estado, setEstado] = useState('');
 
-    const clearInputs = () => {
-        setUserId('');
-        setPhone('');
-        setName('');
-        setAge('');
-        setUsuarioId('');
-    };
+  const clearInputs = () => {
+    setCorreo('');
+    setEstado('');
+    setDate('');
+    setMeta('');
+  
+  };
 
 
-    return (
-        <View style={styles.container}>
-            <TextInput
-                style={styles.input}
-                placeholder="userId"
-                onChangeText={(text) => setUserId(text)}
-                value={userId}
-            />
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="correo"
+        onChangeText={(text) => setCorreo(text)}
+        value={correo}
+      />
 
-            <TextInput
-                style={styles.input}
-                placeholder="Nombre"
-                onChangeText={(text) => setName(text)}
-                value={name}
-            />
+      <TextInput
+        style={styles.input}
+        placeholder="meta"
+        onChangeText={(text) => setMeta(text)}
+        value={meta}
+      />
 
-            <TextInput
-                style={styles.input}
-                placeholder="Edad"
-                onChangeText={(text) => setAge(text)}
-                value={age}
-            />
+      <TextInput
+        style={styles.input}
+        placeholder="estado"
+        onChangeText={(text) => setEstado(text)}
+        value={estado}
+      />
 
-            <TextInput
-                style={styles.input}
-                placeholder="Teléfono"
-                onChangeText={(text) => setPhone(text)}
-                value={phone}
-            />
+      <TextInput
+        style={styles.input}
+        placeholder="Date"
+        onChangeText={(text) => setDate(text)}
+        value={date}
+      />
 
-            <Button title='send data' onPress={() => sendDataToFirebase(userId, phone, name, age)}></Button>
+      <Button title='send data' onPress={() => sendDataToFirebase(correo, date, meta, estado)}></Button>
 
-        </View>
-    )
+    </View>
+  )
 }
 
 export default llamadoData;
 
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      paddingHorizontal: 16,
-    },
-    input: {
-      height: 40,
-      borderColor: 'gray',
-      borderWidth: 1,
-      marginBottom: 12,
-      paddingHorizontal: 8,
-    },
-    userContainer: {
-      marginBottom: 12,
-    },
-    userInfo: {
-      fontSize: 16,
-      fontWeight: 'bold',
-    },
-  
-    modalContainer: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    },
-    modalContent: {
-      backgroundColor: 'white',
-      padding: 16,
-      borderRadius: 8,
-      width: '80%',
-    },
-    modalCloseButton: {
-      marginTop: 16,
-      alignSelf: 'flex-end',
-    },
-    modalCloseButtonText: {
-      fontSize: 16,
-      color: 'blue',
-    }, modalContainer: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    },
-    modalContent: {
-      backgroundColor: 'white',
-      padding: 16,
-      borderRadius: 8,
-      width: '80%',
-    },
-    modalCloseButton: {
-      marginTop: 16,
-      alignSelf: 'flex-end',
-    },
-    modalCloseButtonText: {
-      fontSize: 16,
-      color: 'blue',
-    },
-    
-  
-  
-  });
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 8,
+  },
+  userContainer: {
+    marginBottom: 12,
+  },
+  userInfo: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 16,
+    borderRadius: 8,
+    width: '80%',
+  },
+  modalCloseButton: {
+    marginTop: 16,
+    alignSelf: 'flex-end',
+  },
+  modalCloseButtonText: {
+    fontSize: 16,
+    color: 'blue',
+  }, modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 16,
+    borderRadius: 8,
+    width: '80%',
+  },
+  modalCloseButton: {
+    marginTop: 16,
+    alignSelf: 'flex-end',
+  },
+  modalCloseButtonText: {
+    fontSize: 16,
+    color: 'blue',
+  },
+
+
+
+});
